5985a0fbc5b3d63d3afadc2b9501ef6a4ad87aca
Job3-eng <jobbunanga@gmail.com>
2023-07-28 15:45:47 -0700

    team work

--- _printf.c
+++ _printf.c
@@ -22,7 +22,7 @@ int _printf(const char *format, ...)
 
 	va_start(list, format);
 
-	for (i = 0, format && format[i] != '\0'; i++)
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
 		{
@@ -63,7 +63,7 @@ int _printf(const char *format, ...)
 void print_buffer(char buffer[], int *buff_ind)
 {
 	if (*buff_ind > 0)
-		write(1, &buffer[0], buff_ind);
+		write(1, &buffer[0], *buff_ind);
 
 	*buff_ind = 0;
 }
